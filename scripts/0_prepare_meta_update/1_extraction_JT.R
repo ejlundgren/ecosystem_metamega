0c1866a7d41ab4c1cf1df83118e7c4679970a797
Erick Lundgren <erick.lundgren@gmail.com>
2025-10-13 12:36:22 -0600

    added papers

---
+++ scripts/0_prepare_meta_update/1_extraction_JT.R
@@ -0,0 +1,9 @@
+
+
+# Use metaDigitize to extract data
+
+groundhog.library(pkg = c("data.table", "tidyverse",
+                          "metaDigitise"),
+                  date = "2025-04-15")
+
+
